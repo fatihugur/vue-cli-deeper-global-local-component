a08cfdbd3ced5a2024cdc214390c989c4a7ea124
Fatih Ugur <fatihugur@gmail.com>
2021-02-02 15:52:16 +0100

    ManageGoals.vue and ActiveGoals.vue added App.vue passed, two button added

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
   <div>
-    <TheHeader />
-    <!-- <the-header></the-header> -->
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-      <p>{{ slotProps["another-prop"] }}</p>
-    </course-goals>
+    <the-header></the-header>
+    <!-- <TheHeader /> -->
+    <button>Active Goals</button>
+    <button>Manage Goals</button>
+
+    <active-goals></active-goals>
+    <manage-goals></manage-goals>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
 
   data() {
