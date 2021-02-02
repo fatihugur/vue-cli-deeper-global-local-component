72ed28c008fe849b8721272d2ea895b0893117ff
Fatih Ugur <fatihugur@gmail.com>
2021-02-02 14:36:28 +0100

    CourseGoals.vue added imported and registered in App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals></course-goals>
   </div>
 </template>
 
@@ -15,12 +16,13 @@
 import TheHeader from "./components/TheHeader.vue";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
-
+import CourseGoals from "./components/CourseGoals.vue";
 export default {
   components: {
     TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals,
   },
 
   data() {
